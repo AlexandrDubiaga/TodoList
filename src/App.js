6def71e334ff5cada52e65afb70227550a7ed429
Alex <alexandr.dubiaga@gmail.com>
2019-10-30 08:51:34 +0200

    test repo

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends React.Component {
                     <TodoListHeader />
                     <TodoListTasks />
                     <TodoListFooter />
-                    <Hello />
+                    <Hello let="root" />
                 </div>
             </div>
         );
