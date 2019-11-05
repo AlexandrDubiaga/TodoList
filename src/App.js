7a9a5cabd6dffe9b4c3ab5c3676defc5e5b462b8
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2019-11-05 19:59:28 +0200

    add props

--- src/App.js
+++ src/App.js
@@ -5,12 +5,18 @@ import TodoListTasks from "./TodoListTasks";
 import TodoListFooter from "./TodoListFooter";
 
 class App extends React.Component {
+    tasks = [{title: "CSS", isDone: true,priority:'low'},
+        {title: "HTML", isDone: false,priority:'hight'},
+        {title: "JS", isDone: true,priority:'low'},
+        {title: "jQuery", isDone: false,priority:'hight'}
+    ]
     render = () => {
+
         return (
             <div className="App">
                 <div className="todoList">
                     <TodoListHeader />
-                    <TodoListTasks />
+                    <TodoListTasks tasks={this.tasks} />
                     <TodoListFooter />
                 </div>
             </div>
